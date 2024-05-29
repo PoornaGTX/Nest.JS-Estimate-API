75449ccf9d72cadd271c3fe269714c6dec2d10c7
PoornaGTX <onlinepoorna@gmail.com>
2024-05-29 14:37:47 +0530

    feat:report api get estimate api

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -34,4 +34,27 @@ export class GetEstimateDto {
   @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number;
+
+  //   @IsString()
+  //   model: string;
+
+  //   @Transform(({ value }) => parseInt(value))
+  //   @IsNumber()
+  //   @Min(1930)
+  //   @Max(2050)
+  //   year: number;
+
+  //   @Transform(({ value }) => parseInt(value))
+  //   @IsNumber()
+  //   @Min(0)
+  //   @Max(1000000)
+  //   mileage: number;
+
+  //   @Transform(({ value }) => parseFloat(value))
+  //   @IsLongitude()
+  //   lng: number;
+
+  //   @Transform(({ value }) => parseFloat(value))
+  //   @IsLatitude()
+  //   lat: number;
 }
